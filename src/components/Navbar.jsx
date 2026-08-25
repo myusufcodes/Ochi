b37ac8d8f16dea0ba55d4a98f194c79d15a7cf1e
myusufcodes <yusuf.dev.pk@gmail.com>
2026-08-26 00:10:32 +0500

    Updated Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,35 @@
+'use client'
 import { navLinks } from "../constants"
 import NavBtn from "./NavBtn"
+import { useRef, useEffect } from "react"
 
 const Navbar = () => {
-    
-    return (
-        <nav className='w-screen flex justify-between px-12 py-4 fixed top-0 z-50 text-[#212121]'>
-            <a href="#"><img src="/logo.svg" alt="Logo" /></a>
+    const navRef = useRef(null)
+    const lastScroll = useRef(0);
+    useEffect(() => {
+        function handleScroll() {
+            const currentScroll = window.scrollY
 
+            const delta = currentScroll - lastScroll.current
+            if (delta > 0) {
+                navRef.current.style.top = '-100px'
+            }
+            if (delta < 0) {
+                navRef.current.style.top = '0px'
+            } if (delta === 0) {
+                navRef.current.style.top = '0px'
+            }
+
+            lastScroll.current = currentScroll
+        }
+
+        window.addEventListener('scroll', handleScroll)
+
+        return () => window.removeEventListener('scroll', handleScroll)
+    }, [])
+    return (
+        <nav ref={navRef} className='w-screen flex justify-between px-12 py-4 fixed top-0 z-50 transition-all duration-150 text-[#212121] bg-white/30 backdrop-blur-md'>
+            <a href="#"><img src="/logo.svg" alt="Logo" /></a>
             <ul className='flex gap-8'>
                 {navLinks.map(({ title, href }) => (
                     <li key={title} className="flex justify-center items-center text-lg">
@@ -14,7 +37,6 @@ const Navbar = () => {
                     </li>
                 ))}
             </ul>
-
             <NavBtn title='Contact Us' href="#" />
         </nav>
     )
